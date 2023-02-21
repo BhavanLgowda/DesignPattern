b7cca46076d18b13922d37b201b05bea62f9cc10
BhavanLgowda <bhavangowda.l@nichi.com>
2023-02-21 17:09:49 +0530

    initial

---
+++ AbstractFactory/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDemo
+{
+    public class Dog : IAnimal
+    {
+        public string Speak()
+        {
+            return "Bark bark";
+        }
+    }
+}
