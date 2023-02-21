ba20a196bf92355380eaa30dd5a04ddc0eb65cd3
BhavanLgowda <bhavangowda.l@nichi.com>
2023-02-21 17:45:54 +0530

    initial

---
+++ Factory/MoneyBank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    internal class MoneyBank : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBank";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+}
