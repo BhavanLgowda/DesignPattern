34e067b545a170a87b57bf3b4904fd1ba6c09e98
BhavanLgowda <bhavangowda.l@nichi.com>
2023-02-21 18:02:03 +0530

    initial

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                ()=> StudentPrinter(),
+                ()=> EmployeePrinter()
+                );
+            Console.ReadLine();
+        }
+
+        private static void EmployeePrinter()
+        {
+            Singleton fromstudent = Singleton.GetInstance;
+            fromstudent.PrintDetail("Second demo");            
+        }
+
+        private static void StudentPrinter()
+        {
+            Singleton fromemployee = Singleton.GetInstance;
+            fromemployee.PrintDetail("First demo");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton? Instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if(Instance == null)
+                 Instance= new Singleton();
+                return Instance;
+            }         
+        }
+        private Singleton() 
+        {
+            counter ++;
+            Console.WriteLine("counter value " + counter.ToString());
+        }
+        public void PrintDetail(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
